39cb274e56468900bc8f64a6a72249f2aa4c646d
bassamkdev <bassamkazemi.dev@gmail.com>
2020-05-18 21:54:53 -0700

    added an expect function

--- jest.js
+++ jest.js
@@ -3,6 +3,14 @@ const { multiply, divison } = require('./math.js');
 let result = multiply(4,5);
 let expected = 20 
 
-if (result !== expected) {
-    throw new Error(`${result} is not equal to ${expected}`)
+expect(result).toBe(expected);
+
+function expect (output) {
+    return {
+        toBe (expected) {
+            if (output !== expected) {
+                throw new Error(`${result} is not equal to ${expected}`)
+            }
+        }
+    }
 }
